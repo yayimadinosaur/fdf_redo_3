4e57c1bd8ac012f79e481bbd72029dc0e51d784a
warren fung <wfung@e1z2r6p1.42.us.org>
2017-06-27 16:24:07 -0700

    rewrote main (in psuedo)

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/27 13:29:37 by wfung             #+#    #+#             */
-/*   Updated: 2017/06/27 13:30:32 by wfung            ###   ########.fr       */
+/*   Updated: 2017/06/27 16:23:40 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 int		main(int ac, char **av)
 {
-	if (ac == 
+	if (ac == 1)
+		ft_putstr("Please include .fdf file\n");
+	if (ac == 2)
+	{
+		if (parse_fdf(av[1]) == 1)
+		{
+			if (create_grid(av[1]) == 0)
+				//free(grid?
+			else
+				if (store_pts(av[1]) == 0)
+					//free stored_pts
+				else
+					draw_fdf();
+		}
+		else
+			ft_putstr(".fdf file invalid\n");
+	}
+	return (0);
 }
