5e1415f18b2eea629f55b9a22db39be424d08d53
warren fung <wfung@e1z2r6p1.42.us.org>
2017-06-27 18:07:29 -0700

    updated?

--- parse_fdf.c
+++ parse_fdf.c
@@ -6,15 +6,28 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/27 16:46:05 by wfung             #+#    #+#             */
-/*   Updated: 2017/06/27 17:44:41 by wfung            ###   ########.fr       */
+/*   Updated: 2017/06/27 18:07:08 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static int		parse_file(int fd)
+static int		count_chr(char *str)
 {
 
+}
+
+static int		parse_file(int fd)
+{
+	int		row;
+	int		col;
+
+	row = 0;
+	col = 0;
+	while (get_next_line(fd, &buff) == 1)
+	{
+
+	}
 }
 
 static int		parse_fn(char *str)
@@ -37,7 +50,7 @@ int		parse_fdf(char *str)
 		ft_putstr(".fdf filename invalid\n");
 		return (0);
 	}
-	if (parse_file(fd) != 1)
+	if (parse_file(0) != 1)
 	{
 		ft_putstr(".fdf file contents invalid\n");
 		return (0);
