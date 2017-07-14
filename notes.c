7af0c1aaa351da20eb23882bc1a2e7e8f5ef48f4
warren fung <wfung@e1z2r6p1.42.us.org>
2017-07-14 15:47:21 -0700

    updated notes

--- notes.c
+++ notes.c
@@ -1,5 +1,6 @@
-//for parse contents
-//	need to double check how many spaces are needed
+//spacing does not matter, only # of numbers do
+//		row + col has to match
+//
 //
 //for count_num_str
 //	if row < 2 || col < 2
